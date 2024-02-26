acdb209e6ef7e3c4b5305f310a6989da01f62270
birdzero <redviking2@naver.com>
2024-02-27 00:35:18 +0900

    feat: 연관관계 편의 메서드 작성

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -28,4 +28,10 @@ public class Category {
     private Category parent;
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
