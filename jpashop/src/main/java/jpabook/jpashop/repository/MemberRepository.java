d3c934b3544413f88ec7a83c504bf21ec405225e
birdzero <redviking2@naver.com>
2024-02-27 21:17:29 +0900

    feat: 회원 서비스 구현

--- jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,15 +3,16 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Member member){
         em.persist(member);
@@ -21,7 +22,7 @@ public class MemberRepository {
         return em.find(Member.class , id);
     }
 
-    private List<Member> findAll(){
+    public List<Member> findAll(){
         return em.createQuery("select m from Member m", Member.class).getResultList();
     }
 
