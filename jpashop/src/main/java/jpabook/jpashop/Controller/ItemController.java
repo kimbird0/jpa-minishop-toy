c623c0bee691a9048bc23d6862b00ee8ea0dab9e
birdzero <redviking2@naver.com>
2024-02-29 03:47:16 +0900

    feat: 상품목록 구현

--- jpashop/src/main/java/jpabook/jpashop/Controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/Controller/ItemController.java
@@ -2,12 +2,16 @@ package jpabook.jpashop.Controller;
 
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,9 +33,16 @@ public class ItemController {
         book.setName(form.getName());
         book.setPrice(form.getPrice());
         book.setStockQuantity(form.getStockQuantity());
-        book.setIsbn(form.getIsnb());
+        book.setIsbn(form.getIsbn());
 
         itemService.saveItem(book);
         return "redirect:/";
+    }
+
+    @GetMapping ("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 }
